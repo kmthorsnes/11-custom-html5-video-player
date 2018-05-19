e5cc83ac252d27a548da6d286219ea7f026dea22
kalle <kmthorsnes@gmail.com>
2018-05-19 17:10:32 +0200

    adds togglePlay function

--- scripts.js
+++ scripts.js
@@ -3,11 +3,24 @@ const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
-
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('[player__slider]');
 
-// Build functions
+// Build functions //
+function togglePlay() {
+    const method = video.paused ? 'play' : 'pause';
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+// alternative 
+/* function togglePlay() {
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+} */
+
 
 // Hook up event listeners
